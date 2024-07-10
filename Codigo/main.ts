f6e2a3ef6aa431399cd40536c8830e51ad714bd7
MariaGuadalupe <mmeza@minsa.gob.ni>
2024-07-10 13:11:16 -0600

    Integracion de js

---
+++ Codigo/main.ts
@@ -0,0 +1,14 @@
+ let cadena : string = "hola2"
+ let uno : number=10;
+ let dos :number=20;
+ let sum : number;
+
+ sum = uno+dos;
+
+
+
+
+
+
+/*let  prueba :  string = "hola"*/
+console.log(`numero: ${sum}`)
---
+++ Codigo/out/main.js
@@ -0,0 +1,8 @@
+var cadena = "hola2";
+var uno = 10;
+var dos = 20;
+var sum;
+sum = uno + dos;
+/*let  prueba :  string = "hola"*/
+console.log("numero: ".concat(sum));
+//# sourceMappingURL=main.js.map
